61f3916ea58a7860940d3c74f2e19835ff5f5008
Agyemang David <ddagyemang3@gmail.com>
2026-06-03 09:17:36 +0000

    chore: updatethe Footer.tsx

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import styles from "./Footer.module.css";
+import styles from "./footer.module.css";
 
 const EXPLORE_LINKS = [
   { href: "/", label: "Home" },
