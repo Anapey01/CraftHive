0b842f73b06e458c896daa1861d157331c974e43
Agyemang David <ddagyemang3@gmail.com>
2026-06-08 13:55:40 +0000

    Vercel fix

---
+++ src/app/services/custom-framing/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function CustomFramingPage() {
+  return (
+    <main>
+      <h1>Custom Framing</h1>
+      <p>Welcome to the custom framing service page.</p>
+    </main>
+  );
+}
---
+++ src/app/services/shadow-box/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function ShadowBox() {
+  return (
+    <main>
+      <h1>Custom Framing</h1>
+      <p>Welcome to the custom framing service page.</p>
+    </main>
+  );
+}
