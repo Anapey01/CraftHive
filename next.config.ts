f7685f9613c61bec87346b8610f7d0f4e990dd0d
Anapey01 <gabriel.k.anapey@gmail.com>
2026-06-30 07:16:18 +0000

    fix(Performance): disable Next.js Image Optimization to fix 60-second timeouts on Vercel free tier

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   allowedDevOrigins:['3000-firebase-chdemogit-1780509661531.cluster-fbfjltn375c6wqxlhoehbz44sk.cloudworkstations.dev'],
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com" },
       { protocol: "https", hostname: "plus.unsplash.com" },
